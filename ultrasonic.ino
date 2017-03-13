5a47645ba831e161597c73ff2e38e970991cfe21
alexanderisz <iszswaggerz@gmail.com>
2017-03-14 01:18:38 +0800

    basic code

---
+++ ultrasonic.ino
@@ -0,0 +1,26 @@
+/*..........................................
+
+By: Alexander Isz ( Iskandar Putra )
+Facebook: Alexander Isz
+
+...........................................*/
+
+#include<NewPing.h>
+#define UStrig 3
+#define USecho 2
+#define USmax 200
+#define USping 200
+NewPing sonar1(UStrig, USecho, USmax);
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  unsigned int US = sonar1.ping();
+  unsigned int distance = sonar1.convert_cm(US);
+  Serial.println(distance);
+}
+
