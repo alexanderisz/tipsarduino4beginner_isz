0fb5611c1a5931b4f866a8f8465d942f0092bc2a
alexanderisz <iszswaggerz@gmail.com>
2017-03-14 01:59:08 +0800

    basic code

---
+++ humidity_DH22.ino
@@ -0,0 +1,16 @@
+#include <dht.h>
+dht DHT;
+
+void setup() 
+{
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+  int readData = DHT.read22(A0);
+  float h = DHT.humidity;
+  float t = DHT.temperature; 
+  Serial.println(h);
+  Serial.println(t);
+}
